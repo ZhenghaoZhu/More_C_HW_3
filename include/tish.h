14ea434f5678e3acb85eb0e0256d21c995b89b0c
Zhenghao Zhu <zhenzhu@cse376u18.cs.stonybrook.edu>
2021-04-06 01:31:29 -0400

    First steps, setting up some stuff

--- include/tish.h
+++ include/tish.h
@@ -2,11 +2,20 @@
 #define KNRM "\033[0m"
 #define NL "\n"
 
+#define MAX_ARGS 20
+#define ARG_MAX_LENGTH 20
+
 #define debug(S, ...)                                                          \
   do {                                                                         \
     fprintf(stderr, KMAG "DEBUG: %s:%s:%d " KNRM S NL, __extension__ __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);                \
   } while (0)
 
 
+void tish_parseArgs(char** cliArgs);
+void tish_pwd();
+void tish_cd(char* destDir);
+void tish_ls();
+void tish_quit(char** cliArgs);
 
+// Signal Handlers //
 void sigint_handler(int sigNum);
--- src/tish.c
+++ src/tish.c
@@ -2,42 +2,147 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
 #include "tish.h"
 
-bool cliRunning = true;
+sig_atomic_t cliRunning = true;
+char *cliArgs = NULL;
+// TODO  Next line for '$?' support
+// int lastCommandExit = 0;
+// TODO  Next for line for '-t' EC
+// bool timeCounting = false;
 
-int main(void){
+void sigint_handler(int sigNum){
+    debug("%i", sigNum);
+    cliRunning = false;
+    return;
+}
+
+int main(int argc, char const *argv[], char *envp[]){
     signal(SIGINT, sigint_handler);
-    char *cliArgs = NULL;
+    cliArgs = NULL;
     size_t cliLen;
     int charCount;
     int commandStatus;
     debug("%s", "Huh");
     while(cliRunning){
-        fprintf(stdout , "%s", "tish> ");
+        printf("%s", "tish> ");
         fflush(stdout);
         charCount = getline(&cliArgs, &cliLen, stdin);
+        if(!cliRunning){
+            break;
+        }
         if(charCount != -1){
             if(!strncmp(cliArgs, "exit", 4)){
-                cliRunning = false;
+                tish_quit(&cliArgs);
             } else {
-                printf("You inputted: %s", cliArgs);
+                tish_parseArgs(&cliArgs);
             }
         } else {
             printf("No \n");
             cliRunning = false;
         }
     }
+
     if(cliArgs != NULL){
         free(cliArgs);
+        cliArgs = NULL;
     }
-    return 1;
+
+    return 0;
 }
 
-void sigint_handler(int sigNum){
-  printf("\nInside handler function\n");
-  cliRunning = false;
-  return;
+void tish_parseArgs(char** cliArgs){
+    char* parsedArgs[MAX_ARGS];
+    char* token;
+    const char delim[2] = " ";
+    int argCount = 0;
+    bool redIn = false;
+    bool redOut = false;
+    bool redErr = false;
+    token = strtok(*cliArgs, delim);
+    // TODO  First set all of the redirections then go through commands in separate while loop
+    /* walk through other tokens */
+    while(token != NULL) {
+        if(redIn){
+            if(freopen(token, "w", stdin) == NULL){
+                fprintf(stderr, "freopen failed\n");
+                break;
+            }
+            redIn = false;
+        } else if(redOut){
+            if(freopen(token, "w", stdout) == NULL){
+                fprintf(stderr, "freopen failed\n");
+                break;
+            }
+            redOut = false;
+        } else if(redErr){
+            if(freopen(token, "w", stderr) == NULL){
+                fprintf(stderr, "freopen failed\n");
+                break;
+            }
+        }
+         else if(token[0] == '$'){
+             // TODO  Add 'echo' check
+            char* skipPointer = token + sizeof(char)*1;
+            char* envContainer = malloc(strlen(skipPointer) - 1);
+            if(envContainer == NULL){
+                fprintf(stderr, "malloc failed\n");
+                break;
+            }
+            strncpy(envContainer, skipPointer,strlen(skipPointer) - 1);
+            printf("%s\n", getenv(envContainer));
+            free(envContainer);
+        } else if(strcmp(token, "<") == 0){
+            redIn = true;
+        } else if(strcmp(token, ">") == 0){
+            redOut = true;
+        } else if(strcmp(token, "2>") == 0){
+            redErr = true;
+        } else {
+            if(strcmp(token, "cd") == 0){
+                token = strtok(NULL, delim);
+                printf("CD TOKEN : %s", token);
+                tish_cd(token);
+            } else if(strcmp(token, "ls")){
+                tish_ls();
+            } else if(strcmp(token, "pwd")){
+                tish_pwd();
+            } else {
+                printf("Other stuff");
+            }
+            parsedArgs[argCount++] = token;
+        }
+        token = strtok(NULL, delim);
+    }
+
+    // for(int i = 0; i < argCount; i++){
+    //     printf("what %s\n", parsedArgs[i]);
+    // }
+    if(argCount > 0){
+        printf("%s", parsedArgs[0]);
+    }
+}
+
+void tish_pwd(){
+    return;
+}
+
+void tish_cd(char* destDir){
+    return;
+}
+
+void tish_ls(){
+    return;
+}
+
+void tish_quit(char** cliArgs){
+    free(*cliArgs);
+    *cliArgs = NULL;
+    cliRunning = false;
+    exit(0);
 }
